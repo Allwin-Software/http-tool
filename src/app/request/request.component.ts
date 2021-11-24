76ca8292f1aad1968ed98e101da3797c9ca51863
Ravin Singh D <ravinsinghd@outlook.com>
2021-11-24 12:18:28 +0530

    Get request done

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-request',
@@ -8,21 +10,50 @@ import { Component, OnInit } from '@angular/core';
 export class RequestComponent implements OnInit {
   methods = [
     {
-      id: 'post',
-      text: 'Post',
-    },
-    {
       id: 'get',
       text: 'Get',
     },
+    {
+      id: 'post',
+      text: 'Post',
+    },
   ];
   headers: any[] = [];
-  constructor() {}
+  requestForm = this.formBuilder.group({
+    method: [this.methods[0]],
+    url: [''],
+  });
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {}
 
   ngOnInit(): void {}
 
   onAuthUpdated(authType: string, data: any) {
     const header = { key: 'Authorization', value: `${authType} ${data}` };
     this.headers.push(header);
+  }
+
+  send() {
+    const formValue = this.requestForm.value;
+    const method = formValue.method;
+    let request = null;
+    switch (method.id) {
+      case 'get': {
+        request = this.httpGet(formValue);
+        break;
+      }
+    }
+
+    request?.subscribe((result) => {
+      console.log(result);
+    });
+  }
+
+  httpGet(formValue: any) {
+    const httpHeaders = this.headers.reduce((coll, header) => {
+      coll[header.key] = header.value;
+      return coll;
+    }, {});
+    return this.httpClient.get(formValue.url, { headers: new HttpHeaders(httpHeaders) });
   }
 }
--- src/app/request/request.module.ts
+++ src/app/request/request.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -23,6 +25,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -31,6 +34,7 @@ const routes: Routes = [
     MatTabsModule,
     BasicAuthenticationModule,
     HeadersModule,
+    HttpClientModule,
   ],
   declarations: [RequestComponent],
 })
